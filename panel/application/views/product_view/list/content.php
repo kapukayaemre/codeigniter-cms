c44494a5a6ff6ae4972aac330011bb1941944078
Emre <kapukayaemre@hotmail.com>
2023-01-31 14:07:09 +0300

    Toggle Butonu Eklendi. Yeni Ekle Butonu Eklendi. Kayıt Yoktur Alert Eklendi

--- panel/application/views/product_view/list/content.php
+++ panel/application/views/product_view/list/content.php
@@ -1,11 +1,17 @@
 <div class="row">
     <div class="col-md-12">
-        <h4 class="m-b-lg">Ürün Listesi</h4>
+        <h4 class="m-b-lg">
+            Ürün Listesi
+            <a href="" class="btn btn-outline btn-primary btn-sm pull-right"><i class="fa fa-plus"></i> Yeni Ekle</a>
+        </h4>
     </div>
     <div class="col-md-12">
         <div class="widget p-lg">
-                For basic styling—light padding and only horizontal dividers—add the base class <code>.table</code> to any <code>&lt;table&gt;</code>. It may seem super redundant, but given the widespread use of tables for other plugins like calendars and date pickers, we've opted to isolate our custom table styles.
-            </p>
+
+            <div class="alert alert-info text-center">
+                <h5 class="alert-title">Kayıt Bulunamadı</h5>
+                <p>Burada herhangi bir veri bulunmamaktadır. Eklemek için lütfen <a href="#">tıklayınız.</a></p>
+            </div>
 
             <table class="table table-hover table-striped">
                 <thead>
@@ -22,7 +28,51 @@
                         <td>monitor-askisi</td>
                         <td>Monitör Askısı</td>
                         <td>360 Derece kullanılabilen monitör askısıdır.</td>
-                        <td>1/0</td>
+                        <td>
+                            <input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
+                        </td>
+                        <td>
+                            <a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i> Sil </a>
+                            <a href="#" class="btn btn-sm btn-warning btn-outline"><i class="fa fa-pencil-square-o"></i> Düzenle </a>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>#1</td>
+                        <td>monitor-askisi</td>
+                        <td>Monitör Askısı</td>
+                        <td>360 Derece kullanılabilen monitör askısıdır.</td>
+                        <td>
+                            <input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
+                        </td>
+                        <td>
+                            <a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i> Sil </a>
+                            <a href="#" class="btn btn-sm btn-warning btn-outline"><i class="fa fa-pencil-square-o"></i> Düzenle </a>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>#1</td>
+                        <td>monitor-askisi</td>
+                        <td>Monitör Askısı</td>
+                        <td>360 Derece kullanılabilen monitör askısıdır.</td>
+                        <td>
+                            <input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
+                        </td>
+                        <td>
+                            <a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i> Sil </a>
+                            <a href="#" class="btn btn-sm btn-warning btn-outline"><i class="fa fa-pencil-square-o"></i> Düzenle </a>
+                        </td>
+                    </tr
+
+                    <tr>
+                        <td>#1</td>
+                        <td>monitor-askisi</td>
+                        <td>Monitör Askısı</td>
+                        <td>360 Derece kullanılabilen monitör askısıdır.</td>
+                        <td>
+                            <input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked />
+                        </td>
                         <td>
                             <a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i> Sil </a>
                             <a href="#" class="btn btn-sm btn-warning btn-outline"><i class="fa fa-pencil-square-o"></i> Düzenle </a>
