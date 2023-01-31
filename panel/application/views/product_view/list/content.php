c211f58976ba30dec5ebc95d00f96aedba89f595
Emre <kapukayaemre@hotmail.com>
2023-01-31 13:15:32 +0300

    Urunler modulunun urun listesi icin html sablonu olusturuldu

--- panel/application/views/product_view/list/content.php
+++ panel/application/views/product_view/list/content.php
@@ -1,1 +1,35 @@
-ürün listesi content
+<div class="row">
+    <div class="col-md-12">
+        <h4 class="m-b-lg">Ürün Listesi</h4>
+    </div>
+    <div class="col-md-12">
+        <div class="widget p-lg">
+                For basic styling—light padding and only horizontal dividers—add the base class <code>.table</code> to any <code>&lt;table&gt;</code>. It may seem super redundant, but given the widespread use of tables for other plugins like calendars and date pickers, we've opted to isolate our custom table styles.
+            </p>
+
+            <table class="table table-hover table-striped">
+                <thead>
+                    <th>#id</th>
+                    <th>url</th>
+                    <th>Başlık</th>
+                    <th>Açıklama</th>
+                    <th>Durumu</th>
+                    <th>İşlem</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>#1</td>
+                        <td>monitor-askisi</td>
+                        <td>Monitör Askısı</td>
+                        <td>360 Derece kullanılabilen monitör askısıdır.</td>
+                        <td>1/0</td>
+                        <td>
+                            <a href="#" class="btn btn-sm btn-danger btn-outline"><i class="fa fa-trash"></i> Sil </a>
+                            <a href="#" class="btn btn-sm btn-warning btn-outline"><i class="fa fa-pencil-square-o"></i> Düzenle </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div><!-- .widget -->
+    </div>
+</div>
