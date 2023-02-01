c946a46a6b80a91891ca9dd8e06d48d73faef2c1
Emre <kapukayaemre@hotmail.com>
2023-02-01 03:05:40 +0300

    SweetAlert ile Confirm kullanılarak kayıt silme işlemi gerçekleştirildi

--- panel/assets/assets/js/custom.js
+++ panel/assets/assets/js/custom.js
@@ -1,8 +1,24 @@
 $(document).ready(function (){
-    Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: 'Something went wrong!',
-        footer: '<a href="">Why do I have this issue?</a>'
+
+    $(".remove-btn").click(function (e){
+                            //attr("data-url") ile aynı işlev
+        var $data_url = $(this).data("url");
+
+
+        Swal.fire({
+            title: 'Emin misiniz?',
+            text: "Bu İşlemi Geri Alamayacaksınız!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Evet, Sil!',
+            cancelButtonText: 'Hayır'
+        }).then(function (result) {
+            if (result.isConfirmed) {
+                window.location.href = $data_url;
+            }
+        });
     })
+
 });
