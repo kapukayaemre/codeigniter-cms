21e0c1de86e999349246d8ffa2cb5aea8dfe5ac2
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-03-09 03:24:59 +0300

    ürün fotoğrafları için isActiveSetter işlemleri yapıldı

--- panel/assets/assets/js/custom.js
+++ panel/assets/assets/js/custom.js
@@ -20,14 +20,18 @@ $(document).ready(function () {
     });
   });
 
-  $(".isActive").change(function () {
-    var $data = $(this).prop("checked");
-    var $data_url = $(this).data("url");
+  $(".content-container, .image_list_container").on(
+    "change",
+    ".isActive",
+    function () {
+      var $data = $(this).prop("checked");
+      var $data_url = $(this).data("url");
 
-    if (typeof $data !== "undefined" && typeof $data_url !== "undefined") {
-      $.post($data_url, { data: $data }, function (response) {});
+      if (typeof $data !== "undefined" && typeof $data_url !== "undefined") {
+        $.post($data_url, { data: $data }, function (response) {});
+      }
     }
-  });
+  );
 
   $(".image_list_container").on("change", ".isCover", function () {
     var $data = $(this).prop("checked");
