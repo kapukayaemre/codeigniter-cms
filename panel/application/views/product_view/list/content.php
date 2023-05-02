a153705989cc5ceb3d5700c6c754f83ccc52d052
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-05-03 00:17:17 +0300

    ürünler modülünün ürün listesi için html şablonu oluşturuldu

--- panel/application/views/product_view/list/content.php
+++ panel/application/views/product_view/list/content.php
@@ -1,1 +1,40 @@
-Ürün Listesi
+<div class="row">
+    <div class="col-md-12">
+        <div class="widget">
+            <header class="widget-header">
+                <h4 class="widget-title">Ürün Listesi</h4>
+            </header><!-- .widget-header -->
+            <hr class="widget-separator">
+            <div class="widget-body">
+                <div class="table-responsive">
+                    <table id="default-datatable" data-plugin="DataTable" class="table table-striped" cellspacing="0"
+                           width="100%">
+                        <thead>
+                        <tr>
+                            <th>#id</th>
+                            <th>URL</th>
+                            <th>Başlık</th>
+                            <th>Açıklama</th>
+                            <th>Durumu</th>
+                            <th>İşlem</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>#1</td>
+                            <td>monitor-askisi</td>
+                            <td>Monitör Askısı</td>
+                            <td>360 derece kullanılabilen monitör askısıdır</td>
+                            <td>1/0</td>
+                            <td>
+                                <a href="#" class="btn btn-danger btn-outline"><i class="fa fa-trash"></i> Sil</a>
+                                <a href="#" class="btn btn-info btn-outline"><i class="fa fa-pencil-square"></i> Düzenle</a>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div><!-- .widget-body -->
+        </div><!-- .widget -->
+    </div>
+</div>
