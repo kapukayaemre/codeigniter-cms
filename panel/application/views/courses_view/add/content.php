a30bb5e64899ed5d801cf4237f287950f5308565
Emre Kapukaya <kapukayaemre@hotmail.com>
2023-05-05 14:12:09 +0300

    eğitimler modülü oluşturuldu ve eğitimler listelendi

---
+++ panel/application/views/courses_view/add/content.php
@@ -0,0 +1,40 @@
+<div class="row">
+    <div class="col-md-12">
+        <h4 class="m-b-lg">
+            Yeni Referans Ekle
+        </h4>
+    </div>
+    <div class="col-md-12">
+        <div class="widget">
+            <div class="widget-body">
+                <form action="<?php echo base_url("references/save"); ?>" method="post" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label>Başlık</label>
+                        <input class="form-control" placeholder="Başlık" name="title"
+                               value="<?php echo isset($_POST["title"]) ? $_POST["title"] : ""; ?>">
+                        <?php if (isset($form_error)) { ?>
+                            <small class="pull-right input-form-error"> <?php echo form_error("title"); ?></small>
+                        <?php } ?>
+                    </div>
+
+                    <div class="form-group">
+                        <label>Açıklama</label>
+                        <textarea
+                            name="description"
+                            class="m-0"
+                            data-plugin="summernote"
+                            data-options="{height: 250}"><?php echo isset($_POST["description"]) ? $_POST["description"] : ""; ?></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <label>Görsel Seçiniz</label>
+                        <input type="file" name="img_url" class="form-control">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary btn-md btn-outline">Kaydet</button>
+                    <a href="<?php echo base_url("references"); ?>" class="btn btn-md btn-danger btn-outline">İptal</a>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ panel/application/views/courses_view/list/content.php
@@ -0,0 +1,72 @@
+<div class="row">
+    <div class="col-md-12">
+        <h4 class="m-b-lg">
+            Eğitim Listesi
+            <a href="<?php echo base_url("courses/new_form"); ?>" class="btn btn-outline btn-primary btn-sm pull-right"><i
+                    class="fa fa-plus"></i> Yeni Ekle</a>
+        </h4>
+        <div class="widget p-lg">
+
+            <?php if (empty($items)) { ?>
+                <div class="alert alert-info text-center">
+                    <h4 class="alert-title">Kayıt Bulunamadı</h4>
+                    <p>Burada herhangi bir veri bulunmamaktadır. Eklemek için lütfen <a
+                            href="<?php echo base_url("courses/new_form"); ?>"><strong>tıklayınız.</strong></a></p>
+                </div>
+            <?php } else { ?>
+                <div class="widget-body">
+                    <div class="table-responsive">
+                        <table id="default-datatable" data-plugin="DataTable"
+                               class="table table-hover table-bordered table-striped content-container">
+                            <thead>
+                            <tr>
+                                <th></th>
+                                <th class="w50">#id</th>
+                                <th>Başlık</th>
+                                <th>Tarih</th>
+                                <th>Açıklama</th>
+                                <th>Görsel</th>
+                                <th>Durumu</th>
+                                <th style="width: 100px">İşlem</th>
+                            </tr>
+                            </thead>
+                            <tbody class="sortable" data-url="<?php echo base_url("courses/rankSetter"); ?>">
+                            <?php foreach ($items as $item) { ?>
+                                <tr id="ord-<?php echo $item->id; ?>">
+                                    <td class="order"><i class="fa fa-reorder"></i></td>
+                                    <td class="w50">#<?php echo $item->id; ?></td>
+                                    <td><?php echo $item->title; ?></td>
+                                    <td><?php echo $item->event_date; ?></td>
+                                    <td><?php echo $item->description; ?></td>
+                                    <td class="text-center" style="width: 75px">
+                                        <img width="100" src="<?php echo base_url("uploads/$viewFolder/$item->img_url"); ?>" alt="" class="img-rounded">
+                                    </td>
+                                    <td class="w50">
+                                        <input
+                                            data-url="<?php echo base_url("courses/isActiveSetter/$item->id"); ?>"
+                                            class="isActive"
+                                            type="checkbox"
+                                            data-switchery
+                                            data-color="#10c469"
+                                            <?php echo ($item->isActive) ? "checked" : "" ?>
+                                        />
+                                    </td>
+                                    <td style="width: 200px" class="text-center">
+                                        <button
+                                            data-url="<?php echo base_url("courses/delete/$item->id"); ?>"
+                                            class="btn btn-danger btn-outline remove-btn">
+                                            <i class="fa fa-trash"></i> Sil
+                                        </button>
+                                        <a href="<?php echo base_url("courses/update_form/$item->id"); ?>"
+                                           class="btn btn-info btn-outline"><i class="fa fa-pencil-square"></i> Düzenle</a>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            <?php } ?>
+        </div><!-- .widget -->
+    </div>
+</div>
